3dc1d85e5c7eb6c81a728977509863870c0f9314
Joao Victor Miranda <jvdmmiranda@gmail.com>
2017-01-04 13:49:13 -0200

    Create a separeted file to data.

--- radar/radar.js
+++ radar/radar.js
@@ -1,155 +1,10 @@
-  var sample_data = [
-    {"year": 2009, "gender": "Female", "cbo": "Comércio", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Administração Pública", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Indústrias de Transformação", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Atividades Administrativas", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Construção", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Transporte e correio", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Saúde e Serviços Sociais", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Educação", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Alojamento e Alimentação", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Agropecuária", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Outros Serviços", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Serviços Especializados", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Atividades Financeiras", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Informação e Comunicação", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Saneamento Básico", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Indústrias Extrativas", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Artes, Cultura e Recreação", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Atividades Imobiliárias", "employee_count": 2},
-    {"year": 2009, "gender": "Female", "cbo": "Eletricidade e Gás", "employee_count": 4},
-    {"year": 2009, "gender": "Female", "cbo": "Serviços Domésticos", "employee_count": 7},
-    {"year": 2009, "gender": "Female", "cbo": "Organismos Internacionais", "employee_count": 2},
-
-    {"year": 2009, "gender": "Male", "cbo": "Comércio", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Administração Pública", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Indústrias de Transformação", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Atividades Administrativas", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Construção", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Transporte e correio", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Saúde e Serviços Sociais", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Educação", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Alojamento e Alimentação", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Agropecuária", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Outros Serviços", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Serviços Especializados", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Atividades Financeiras", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Informação e Comunicação", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Saneamento Básico", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Indústrias Extrativas", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Artes, Cultura e Recreação", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Atividades Imobiliárias", "employee_count": 8},
-    {"year": 2009, "gender": "Male", "cbo": "Eletricidade e Gás", "employee_count": 6},
-    {"year": 2009, "gender": "Male", "cbo": "Serviços Domésticos", "employee_count": 3},
-    {"year": 2009, "gender": "Male", "cbo": "Organismos Internacionais", "employee_count": 8},
-
-    {"year": 2010, "gender": "Female", "cbo": "Comércio", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Administração Pública", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Indústrias de Transformação", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Atividades Administrativas", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Construção", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Transporte e correio", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Saúde e Serviços Sociais", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Educação", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Alojamento e Alimentação", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Agropecuária", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Outros Serviços", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Serviços Especializados", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Atividades Financeiras", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Informação e Comunicação", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Saneamento Básico", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Indústrias Extrativas", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Artes, Cultura e Recreação", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Atividades Imobiliárias", "employee_count": 3},
-    {"year": 2010, "gender": "Female", "cbo": "Eletricidade e Gás", "employee_count": 5},
-    {"year": 2010, "gender": "Female", "cbo": "Serviços Domésticos", "employee_count": 8},
-    {"year": 2010, "gender": "Female", "cbo": "Organismos Internacionais", "employee_count": 3},
-
-    {"year": 2010, "gender": "Male", "cbo": "Comércio", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Administração Pública", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Indústrias de Transformação", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Atividades Administrativas", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Construção", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Transporte e correio", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Saúde e Serviços Sociais", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Educação", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Alojamento e Alimentação", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Agropecuária", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Outros Serviços", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Serviços Especializados", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Atividades Financeiras", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Informação e Comunicação", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Saneamento Básico", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Indústrias Extrativas", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Artes, Cultura e Recreação", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Atividades Imobiliárias", "employee_count": 9},
-    {"year": 2010, "gender": "Male", "cbo": "Eletricidade e Gás", "employee_count": 7},
-    {"year": 2010, "gender": "Male", "cbo": "Serviços Domésticos", "employee_count": 4},
-    {"year": 2010, "gender": "Male", "cbo": "Organismos Internacionais", "employee_count": 9},
-
-    {"year": 2011, "gender": "Female", "cbo": "Comércio", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Administração Pública", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Indústrias de Transformação", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Atividades Administrativas", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Construção", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Transporte e correio", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Saúde e Serviços Sociais", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Educação", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Alojamento e Alimentação", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Agropecuária", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Outros Serviços", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Serviços Especializados", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Atividades Financeiras", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Informação e Comunicação", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Saneamento Básico", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Indústrias Extrativas", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Artes, Cultura e Recreação", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Atividades Imobiliárias", "employee_count": 4},
-    {"year": 2011, "gender": "Female", "cbo": "Eletricidade e Gás", "employee_count": 6},
-    {"year": 2011, "gender": "Female", "cbo": "Serviços Domésticos", "employee_count": 9},
-    {"year": 2011, "gender": "Female", "cbo": "Organismos Internacionais", "employee_count": 4},
-
-    {"year": 2011, "gender": "Male", "cbo": "Comércio", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Administração Pública", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Indústrias de Transformação", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Atividades Administrativas", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Construção", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Transporte e correio", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Saúde e Serviços Sociais", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Educação", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Alojamento e Alimentação", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Agropecuária", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Outros Serviços", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Serviços Especializados", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Atividades Financeiras", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Informação e Comunicação", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Saneamento Básico", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Indústrias Extrativas", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Artes, Cultura e Recreação", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Atividades Imobiliárias", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Eletricidade e Gás", "employee_count": 8},
-    {"year": 2011, "gender": "Male", "cbo": "Serviços Domésticos", "employee_count": 5},
-    {"year": 2011, "gender": "Male", "cbo": "Organismos Internacionais", "employee_count": 8},
-  ];
-
-  var sample_data_comercio = [
-        {"gender": "Female", "cbo": "Comercio varejista", "employee_count":30},
-        {"gender": "Female", "cbo": "Comercio atacadista", "employee_count":21},
-        {"gender": "Female", "cbo": "Comercio de Automotores", "employee_count":8},
-
-        {"gender": "Male", "cbo": "Comercio varejista", "employee_count":8},
-        {"gender": "Male", "cbo": "Comercio atacadista", "employee_count":21},
-        {"gender": "Male", "cbo": "Comercio de Automotores", "employee_count":30}
-    ];
-   
-  var visualization = d3plus.viz()
+    var visualization = d3plus.viz()
     .container("#viz")
     .data(sample_data)
     .id(["gender", "cbo"])
     .color("gender")
     .size("employee_count")
     .type("radar")
-    .tooltip({"stacked": true})
     .time({"value": "year", "solo": 2009})
 
 
@@ -190,9 +45,9 @@
 
                         }
                         else if(value == "comercio"){
-                            
-
                             viz.data(sample_data_comercio)
+                            .time({"value": "year", "solo": 2010})
+                            .color("gender")
                             .id({
                                 "value": ["gender", "cbo"],
                                 "solo" : []
@@ -200,7 +55,14 @@
                             .draw();
                         }
                         else if(value == "administracao_publica"){
-                            
+                           viz.data(sample_data_administracao_publica)
+                           .time({"value": "year", "solo": 2010})
+                           .color("gender")
+                           .id({
+                                "value": ["gender", "cbo"],
+                                "solo" : []
+                            })
+                            .draw();
                         }
                         else if(value == "industrias_de_transformacao"){
                             
