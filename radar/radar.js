c1634e6919689f4dfaa1d2a0d226f0fc2ce6a17a
Joao Victor Miranda <jvdmmiranda@gmail.com>
2017-01-05 14:48:48 -0200

    Create a directory to scripts and change the data used by radar chart to a data closer to the real one, remove the dropdownlist from the radarchart

--- radar/radar.js
+++ radar/radar.js
@@ -1,142 +1,56 @@
-    var visualization = d3plus.viz()
-    .container("#viz")
-    .data(sample_data)
-    .id(["gender", "cbo"])
-    .color("gender")
-    .size("employee_count")
-    .type("radar")
-    .time({"value": "year", "solo": 2010})
+var data = [];
 
+$(document).ready(function(){
+    ajaxQueue([
+        "http://api.staging.dataviva.info/rais/year/gender/cnae",
+        "http://api.staging.dataviva.info/metadata/ports"
+    ], 
 
-    .ui([{
-                    "label" : "Agregação",
-                    "type" : "drop",
-                    "value" : [
-                        {"Todos": "all"},
-                        {"Comercio": "comercio"},
-                        {"Indústrias de Transformação": "industrias_de_transformacao"},
-                        {"Atividades Administrativas": "atividades_administrativas"},
-                        {"Construção": "construcao"},
-                        {"Transporte e correio": "transporte_correio"},
-                        {"Saúde e Serviços Sociais": "saude_servicos_socias"},
-                        {"Educação": "educacao"},
-                        {"Alojamento e Alimentação": "alojamento_alimentacao"},
-                        {"Agropecuária": "agropecuaria"},
-                        {"Outros Serviços": "outros_servicos"},
-                        {"Serviços Especializados": "servicos_especializados"},
-                        {"Atividades Financeiras": "atividades_financeiras"},
-                        {"Informação e Comunicação": "informacao_comunicacao"},
-                        {"Saneamento Básico": "saneamento_basico"},
-                        {"Indústrias Extrativas": "industrias_extrativas"},
-                        {"Artes, Cultura e Recreação": "artes_cultura_recreacao"},
-                        {"Atividades Imobiliárias": "atividades_imobiliarias"},
-                        {"Eletricidade e Gás": "eletricidade_gas"},
-                        {"Serviços Domésticos": "servicos_domesticos"},
-                        {"Organismos Internacionais": "organismos_internacionais"}
-                    ],
-                    "method" : function(value, viz){                        
-                        if(value == "all"){
-                            viz.data(sample_data)
-                            .id({
-                                "value": ["gender", "cbo"],
-                                "solo" : []
-                            }).depth(0).draw();
+    function(responses){
+        responses = [
+            api,
+            metadata
+        ];
 
-                        }
-                        else if(value == "comercio"){
-                            viz.data(sample_data_comercio)
-                            .time({"value": "year", "solo": 2010})
-                            .color("gender")
-                            .id({
-                                "value": ["gender", "cbo"],
-                                "solo" : []
-                            })
-                            .draw();
-                        }
-                        else if(value == "industrias_de_transformacao"){
-                            viz.data(sample_data_industrias_de_transformacao)
-                            .time({"value": "year", "solo": 2010})
-                            .color("gender")
-                            .id({
-                                "value": ["gender", "cbo"],
-                                "solo" : []
-                            })
-                            .draw();
-                        }
-                        else if(value == "atividades_administrativas"){
-                            viz.data(sample_data_atividades_administrativas)
-                            .time({"value": "year", "solo": 2010})
-                            .color("gender")
-                            .id({
-                                "value": ["gender", "cbo"],
-                                "solo" : []
-                            })
-                            .draw();
-                        }
-                        else if(value == "construcao"){
-                            
-                        }
-                        else if(value == "transporte_correio"){
-                            
-                        }
-                        else if(value == "saude_servicos_socias"){
-                            
-                        }
-                        else if(value == "educacao"){
-                            
-                        }
-                        else if(value == "alojamento_alimentacao"){
-                            
-                        }
-                        else if(value == "agropecuaria"){
-                            
-                        }
-                        else if(value == "outros_servicos"){
-                            
-                        }
-                        else if(value == "servicos_especializados"){
-                            
-                        }
-                        else if(value == "atividades_financeiras"){
-                            
-                        }
-                        else if(value == "informacao_comunicacao"){
-                            
-                        }
-                        else if(value == "saneamento_basico"){
-                            
-                        }
-                        else if(value == "industrias_extrativas"){
-                            
-                        }
-                        else if(value == "artes_cultura_recreacao"){
-                            
-                        }
-                        else if(value == "atividades_imobiliarias"){
-                            
-                        }
-                        else if(value == "eletricidade_gas"){
-                            
-                        }
-                        else if(value == "servicos_domesticos"){
-                            
-                        }
-                        else{
-                            
-                        }
+        responses[0].data.forEach(function(item , index){
+            data.push({
+                "year": item[0],
+                "gender": item[1] == "1" ? "Male" : "Female",
+                "cnae": item[2],
+                "average_monthly_wage": +item[5]
+            });
+        });
 
-                    }
-           }])
+        data.map(function(item){
+            item.name = responses[1].industries[item.cnae].name_pt;
+        });
 
-    .background("transparent")
-    // .title({
-    //     "font": {"align": "left"},
-    //     "padding": 5,
-    //     "sub": {"font": {"align": "left"}},
-    //     "total": {"font": {"align": "left"}}
-    // })
-    // .title("Balança Comercial dos Soja no Brasil (Jan 2000-Mar 2016)")
-    // .title({
-    //     "sub": "Baseado nos Estados Produtores."
-    // })
-    .draw();
+        var visualization = d3plus.viz()
+        .container("#viz")
+        .type("radar")
+        .data(data)
+        .id(["gender", "name"])
+        .color("gender",
+            {"tooltip":("gender")})
+        .size("average_monthly_wage")
+        .time({"value": "year", "solo": []})
+        .tooltip({"value": false})
+        .format({
+            "text": function(text, params) {
+                if (params.key === "0") {
+                    return "Female";
+                }
+                else if (params.key === "1") {
+                    return "Male";
+                }
+                else {
+                    return d3plus.string.title(text, params);
+                }
+            }
+        })
+        .background("transparent")
+        .draw();
+
+    });
+});
+
