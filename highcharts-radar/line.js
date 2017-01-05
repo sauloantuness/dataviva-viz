504be9ededbbf0724415a3854913ef07433991c8
daniel1409 <danieltafuri1998@gmail.com>
2017-01-05 11:36:36 -0200

    add filter by year on chart

--- highcharts-radar/line.js
+++ highcharts-radar/line.js
@@ -1,4 +1,52 @@
-Highcharts.chart('container', {
+var categories = ['Comércio', 'Administração Pública', 'Indústrias de Transformação', 'Atividades Administrativas',
+    'Construção', 'Transporte e correio', 'Saúde e Serviços Sociais', 'Educação', 'Alojamento e Alimentação',
+     'Agropecuária', 'Outros Serviços', 'Serviços Especializados', 'Atividades Financeiras', 'Informação e Comunicação',
+     'Saneamento Básico', 'Indústrias Extrativas', 'Artes, Cultura e Recreação', 'Atividades Imobiliárias', 'Eletricidade e Gás',
+     'Serviços Domésticos', 'Organismos Internacionais'
+];
+
+
+var select_by_year = function(){
+
+    var year = 2009;
+
+    var filtered_data_by_year = sample_data.filter(function(item){
+        return item.year == year;
+    })
+
+    var male_data = sample_data.filter(function(item){
+        return item.gender == 'Male';
+    })
+
+    var female_data = sample_data.filter(function(item){
+        return item.gender == 'Female';
+    })
+
+    var male_series = [];
+    var female_series = [];
+
+    categories.forEach(function(category, index, array){
+
+        var males_by_category = male_data.filter(function(item){
+            return item.cbo == category;
+        })[0];
+
+        var females_by_category = female_data.filter(function(item){
+            return item.cbo == category;
+        })[0];
+
+        male_series.push(males_by_category.employee_count);
+
+        female_series.push(females_by_category.employee_count);
+    });
+
+    chart.series[0].setData(male_series);
+    chart.series[1].setData(female_series);
+}
+
+
+
+var chart = Highcharts.chart('container', {
 
         chart: {
             polar: true,
@@ -14,6 +62,14 @@ Highcharts.chart('container', {
             size: '80%'
         },
 
+        rangeSelector: {
+                button: [{
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }],
+            selected: 3
+        },
         xAxis: {
             categories: ['Comércio', 'Administração Pública', 'Indústrias de Transformação', 'Atividades Administrativas',
                     'Construção', 'Transporte e correio', 'Saúde e Serviços Sociais', 'Educação', 'Alojamento e Alimentação',
@@ -40,6 +96,21 @@ Highcharts.chart('container', {
             verticalAlign: 'top',
             y: 70,
             layout: 'vertical'
+        },
+
+        exporting: {
+            buttons: {
+                customButton: {
+                    text: '2009',
+                    onclick: select_by_year
+                },
+               anotherButton: {
+                    text: 'Filter by year',
+                    onclick: function () {
+                        alert('You pressed the button');
+                    }
+                }
+            }
         },
 
         series: [{
